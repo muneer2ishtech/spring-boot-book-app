e8006be93a3c05981d5fd2bf87518e8e16acf859
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-10-22 03:12:27 +0300

    flyway dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
+	implementation("org.flywaydb:flyway-core")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 
