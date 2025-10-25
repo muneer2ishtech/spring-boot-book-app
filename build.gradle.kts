8bdebc20af52cec46d7c4ce1a02f2fa6fe2895a7
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-10-26 00:26:01 +0300

    dev tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,12 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
+
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-devtools")
 
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
